6d21a787b19b41b3f5ca53d0e4539ffc63a3062b
Chris Z <eathena@hotmail.com>
2018-06-05 06:49:22 -0400

    Updated functionality to use grocery names instead of item numbers

--- src/com/chrisz/GroceryList.java
+++ src/com/chrisz/GroceryList.java
@@ -12,29 +12,60 @@ public class GroceryList {
 
     public void printGroceryList() {
         System.out.println("You have " + groceryList.size() + " items in your grocery list");
-        for(int i=0; i<groceryList.size(); i++) {
-            System.out.println((i+1) + ". " + groceryList.get(i));
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+    // *** This section was added in last lesson *********
+    public void modifyGroceryItem(String oldItem, String newItem) {
+        int position = findItem(oldItem);
+        if(position >= 0) {
+            modifyGroceryItem(position, newItem);
         }
     }
 
-    public void modifyGroceryItem(int position, String newItem) {
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if(position >= 0) {
+            removeGroceryItem(position);
+        }
+    }
+    // ***** and end here ********************************
+
+    // changed to private as override method above accesses this
+    private void modifyGroceryItem(int position, String newItem) {
         groceryList.set(position, newItem);
         System.out.println("Grocery item " + (position + 1) + " has been modified.");
     }
 
-    public void removeGroceryItem(int position) {
+    private void removeGroceryItem(int position) {
         String theItem = groceryList.get(position);
         groceryList.remove(position);
         System.out.println(theItem + " has been removed from your list.");
     }
 
-    public String findItem(String searchItem) {
+    private int findItem(String searchItem) {
+        return groceryList.indexOf(searchItem);
         //boolean exists = groceryList.contains(searchItem);
 
-        int position = groceryList.indexOf(searchItem);
-        if(position>=0) {
-            return groceryList.get(position);
-        }
-        return null;
+//        int position = groceryList.indexOf(searchItem);
+//        if(position>=0) {
+//            return groceryList.get(position);
+//        }
+//        return null;
+    }
+
+    //
+    public boolean onFile(String searchItem) {
+        int position = findItem(searchItem);
+        return (position >= 0);
+//        if(position >= 0) {
+//            return true;
+//        }
+//        return false;
+    }
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
     }
 }
--- src/com/chrisz/Main.java
+++ src/com/chrisz/Main.java
@@ -1,5 +1,6 @@
 package com.chrisz;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,12 +16,12 @@ public class Main {
         boolean quit = false;
         int choice = 0;
         printInstructions();
-        while(!quit) {
+        while (!quit) {
             System.out.println("Enter your choice: ");
             choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch(choice) {
+            switch (choice) {
                 case 0:
                     printInstructions();
                     break;
@@ -40,6 +41,9 @@ public class Main {
                     searchItem();
                     break;
                 case 6:
+                    processArrayList();
+                    break;
+                case 7:
                     quit = true;
                     break;
             }
@@ -63,30 +67,40 @@ public class Main {
     }
 
     public static void modifiyItem() {
-        System.out.print("Enter item number: ");
-        int itemNo = scanner.nextInt();
+        System.out.print("Enter item name: ");
+        String oldItem = scanner.nextLine();
         scanner.nextLine(); // clear the buffer
         System.out.print("Enter replacement item: ");
         String newItem = scanner.nextLine();
         //scanner.nextLine();
-        groceryList.modifyGroceryItem(itemNo-1, newItem);
+        groceryList.modifyGroceryItem(oldItem, newItem);
     }
 
     public static void removeItem() {
-        System.out.print("Enter item number: ");
-        int itemNo = scanner.nextInt();
+        System.out.print("Enter item name: ");
+        String removeItem = scanner.nextLine();
         scanner.nextLine(); // clear the buffer
-        groceryList.removeGroceryItem(itemNo-1);
+        groceryList.removeGroceryItem(removeItem);
     }
 
     public static void searchItem() {
         System.out.print("Enter grocery item to search for: ");
         String searchItem = scanner.nextLine();
-        if(groceryList.findItem(searchItem) != null) {
+        if (groceryList.onFile(searchItem)) {
             System.out.println("Found " + searchItem + " in our grocery list.");
         } else {
             System.out.println(searchItem + " is not in the shopping list.");
         }
+    }
+
+    public static void processArrayList() {
+        ArrayList<String> newArray = new ArrayList<String>();
+        newArray.addAll(groceryList.getGroceryList()); // copy arraylist
+
+        ArrayList<String> nextArray = new ArrayList<String>(groceryList.getGroceryList()); // another way
+
+        String[] myArray = new String[groceryList.getGroceryList().size()]; // initialize String array to size
+        myArray = groceryList.getGroceryList().toArray(myArray);
     }
 }
 
